a225cd16f19990fef13e1ee8fb3f360ac2f85189
Knut Olav Løite <koloite@gmail.com>
2021-11-19 13:38:24 +0100

    feat: add initial version of NHibernate

---
+++ Google.Cloud.Spanner.NHibernate.Tests/Entities/Singer.cs
@@ -0,0 +1,33 @@
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Google.Cloud.Spanner.NHibernate.Tests.Entities
+{
+    public class Singer
+    {
+        public virtual long SingerId { get; set; }
+        
+        public virtual string FirstName { get; set; }
+        
+        public virtual string LastName { get; set; }
+        
+        public virtual string FullName { get; set; }
+        
+        public virtual SpannerDate BirthDate { get; set; }
+        
+        public virtual object Picture { get; set; }
+    }
+
+    public class SingerMapping : ClassMapping<Singer>
+    {
+        public SingerMapping()
+        {
+            Id(x => x.SingerId);
+            Property(x => x.FirstName);
+            Property(x => x.LastName);
+            Property(x => x.FullName, mapper => mapper.Generated(PropertyGeneration.Always));
+            Property(x => x.BirthDate);
+            // Property(x => x.Picture);
+        }
+    }
+}
---
+++ Google.Cloud.Spanner.NHibernate.Tests/NHibernateMockServerFixture.cs
@@ -0,0 +1,58 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.Connection.MockServer;
+using Google.Cloud.Spanner.NHibernate.Tests.Entities;
+using Grpc.Core;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Util;
+using System;
+
+namespace Google.Cloud.Spanner.NHibernate.Tests
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class TestConnectionProvider : SpannerConnectionProvider
+    {
+        public override ChannelCredentials ChannelCredentials { get => ChannelCredentials.Insecure; set => throw new InvalidOperationException(); }
+    }
+    
+    public class NHibernateMockServerFixture : SpannerMockServerFixture
+    {
+        public NHibernateMockServerFixture()
+        {
+            ReflectHelper.ClassForName(typeof(SpannerDriver).AssemblyQualifiedName);
+            var nhConfig = new Configuration().DataBaseIntegration(db =>
+            {
+                db.Dialect<SpannerDialect>();
+                db.ConnectionString = ConnectionString;
+                db.BatchSize = 100;
+                db.ConnectionProvider<TestConnectionProvider>();
+            });
+            var mapper = new ModelMapper();
+            mapper.AddMapping<SingerMapping>();
+            mapper.AddMapping<AlbumMapping>();
+            mapper.AddMapping<TableWithAllColumnTypesMapping>();
+            var mapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+            nhConfig.AddMapping(mapping);
+            
+            SessionFactory = nhConfig.BuildSessionFactory();
+        }
+
+        public ISessionFactory SessionFactory { get; }
+        
+        public string ConnectionString => $"Data Source=projects/p1/instances/i1/databases/d1;Host={Host};Port={Port}";
+    }
+}
